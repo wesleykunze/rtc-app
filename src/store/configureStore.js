40086602d172803aa040d5ae5e8557dab0b70efc
Wesley <wesleykunze@gmail.com>
2018-04-19 20:50:04 -0700

    Setup trois

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,4 +1,4 @@
-import {createStore, combineReducers} from 'Redux';
+import {createStore, combineReducers} from 'redux';
 import expensesReducer from '../reducers/expenses';
 import filtersReducer from '../reducers/filters';
 
